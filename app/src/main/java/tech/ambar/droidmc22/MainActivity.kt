2ca9172d75314f847b940c14aa8a0c8a405549c0
AMBAR <ambar@AMBARs-MacBook-Pro.local>
2022-11-23 18:02:25 +0530

    Create app to add 2 numbers
    Signed-off-by: AMBAR <saurabh.ambar2017@gmail.com>

--- app/src/main/java/tech/ambar/droidmc22/MainActivity.kt
+++ app/src/main/java/tech/ambar/droidmc22/MainActivity.kt
@@ -2,10 +2,34 @@ package tech.ambar.droidmc22
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var btnAdd = findViewById<Button>(R.id.btnAdd)
+        var etNum1 = findViewById<EditText>(R.id.etNum1)
+        var etNum2 = findViewById<EditText>(R.id.etNum2)
+        var tvResult = findViewById<TextView>(R.id.tvResult)
+
+        btnAdd.setOnClickListener{
+            try{
+                var num1 = etNum1.text.toString().toInt()
+                var num2 = etNum2.text.toString().toInt()
+                var sum = num1 + num2
+                tvResult.text = "RESULT: $sum"
+            }catch (e : NumberFormatException) {
+                AlertDialog.Builder(this)
+                    .setTitle("Error")
+                    .setMessage("Please enter a valid number")
+                    .setPositiveButton("OK",null)
+                    .show()
+            }
+        }
     }
 }
